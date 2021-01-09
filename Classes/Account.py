658f0ea7883023ce89bb894c66ec3ad5c4f48e33
Kobi Hancz <kobihancz@Kobis-MacBook-Pro-2.local>
2021-01-08 17:35:47 -0800

    Final submission

--- Classes/Account.py
+++ Classes/Account.py
@@ -1,33 +1,51 @@
-#Checkings & Savings Accounts
-
-#Methods – Withdraw & Deposit
+import json
 
 class Account:
-    def __init__(self, checkings_balance, savings_balance):
-        self.checkings = Checking(checkings_balance)
-        self.savings = Saving(savings_balance)
-        
+    def __init__(self,customer_id, balance):
+       self.balance = balance
+       self.customer_id = customer_id
     
-    def deposit(self, amount):
-        #Deposit amount in account
+    def deposit(self, atype, amount):
+        self.balance = self.balance + amount 
+        return self.balance
+    
+    #update account w/ customer method --> customer.py
+    def update_AccountBalance(self, atype, newbalance):
+        with open("/Users/kobihancz/Desktop/Banking_System_Project/Banking_System_Repo/Database/Accounts.json", 'r') as acctf:
+            file_dict = json.load(acctf)
+            for acct in file_dict["Accounts"]:
+                if "customer_id" in acct and acct["customer_id"] == self.customer_id and acct["account_type"] == atype:
+                    acct["balance"] = newbalance
+                with open("/Users/kobihancz/Desktop/Banking_System_Project/Banking_System_Repo/Database/Accounts.json", 'w') as testf:
+                    json.dump(file_dict,testf)
+                   
 
-    def write_accounts_to_db(self):
+    def write_accounts_to_db(self, atype):
         with open("/Users/kobihancz/Desktop/Banking_System_Project/Banking_System_Repo/Database/Accounts.json", 'r') as accountf:
             file_dict = json.load(accountf)
-            file_dict["Customers"].append(self.__dict__)
+            temp = self.__dict__
+            temp['account_type'] = atype
+            file_dict["Accounts"].append(temp)
         with open("/Users/kobihancz/Desktop/Banking_System_Project/Banking_System_Repo/Database/Accounts.json", 'w') as accountf:
             json.dump(file_dict,accountf)
 
 class Checking(Account):
-    def __init__(self, balance):
-        self.balance = balance 
+    def __init__(self,customer_id, balance):
+        self.balance = balance
+        self.customer_id = customer_id 
 
-    def withdrawl(self, amount):
-        #Withdraw amount from account 
+    def withdrawl(self, atype, amount):
+        self.balance = self.balance - amount 
+        return self.balance
 
 class Saving(Account):
-    def __init__(self, balance):
-        self.balance = balance 
+    def __init__(self,customer_id, balance):
+        self.balance = balance
+        self.customer_id= customer_id
 
-    def transfer(self, amount, checkings_obj):
-        checkings_obj.balance += amount 
+    def transfer(self, amount, checkings):
+        self.balance = self.balance - amount
+        checkings.balance = checkings.balance + amount
+        return self.balance, checkings.balance
+
+         
